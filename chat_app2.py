176410e7e97a0706aeae36109de082532a740de6
DrKamthornTan <150987195+DrKamthornTan@users.noreply.github.com>
2024-04-17 13:38:01 +0700

    Add files via upload

---
+++ chat_app2.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+st.set_page_config(page_title='Meditron Chatbot', layout='wide')
+st.title("DHV AI Startup Meditron + Mistral Chatbot")
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model_id = "malhajar/meditron-7b-chat"
+    model = AutoModelForCausalLM.from_pretrained(model_id,
+                                                 revision="main")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    return model
+
+@st.cache(allow_output_mutation=True)
+def load_tokenizer():
+    model_id = "malhajar/meditron-7b-chat"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    return tokenizer
+
+model = load_model()
+tokenizer = load_tokenizer()
+
+question = st.text_input("Enter your question:")
+prompt = f'''
+### Instruction:
+{question} 
+
+### Response:'''
+
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
+output = model.generate(input_ids, max_length=512, pad_token_id=tokenizer.eos_token_id, top_k=50, do_sample=True, top_p=0.95)
+response = tokenizer.decode(output[0], skip_special_tokens=True)
+
+st.markdown(response)
